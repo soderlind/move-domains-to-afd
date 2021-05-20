f5b079ce8c57ebd9296b6996084a2cfcec9dab62
Per Søderlind <per@soderlind.no>
2021-05-21 01:47:41 +0200

    add routing rules for all AFDs

--- add-afd.sh
+++ add-afd.sh
@@ -12,7 +12,6 @@ az config set extension.use_dynamic_install=yes_without_prompt
 # The variable below are set by the script.
 
 KV_ID=$(az keyvault list --subscription "$SUBSCRIPTION" --resource-group $RG  | jq -r '[.[].id]|join("")')
-OLD_FRONTENDS=$(az network front-door frontend-endpoint list --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD | jq -r '[.[].name]|join(" ")' )
 DNS_ZONES=$(az network dns zone list --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --query '[].name' | jq -r '.|join(" ")')
 
 
@@ -23,12 +22,6 @@ for SECRET_NAME in $SECRET_NAMES; do
 	SECRET_ID=$(az keyvault certificate show --vault-name $KV --name $SECRET_NAME |jq  -r '[.sid]|join("")|split("/")[-1]')
 	for DOMAIN_WITH_CERT in $DOMAINS_WITH_CERTS; do
 
-		FRONTENDPOINT="$(echo $DOMAIN_WITH_CERT | tr "." "-")-frontend-endpoint"
-		if [[ $OLD_FRONTENDS =~ (^|[[:space:]])$FRONTENDPOINT($|[[:space:]]) ]]; then
-			echo -e "\t$DOMAIN_WITH_CERT in Front Door, skipping\n"
-			continue
-		fi
-
 		ZONE=$(echo $DOMAIN_WITH_CERT | rev | cut -d. -f1-2 | rev)
 		if [[ $ZONE != $DOMAIN_WITH_CERT ]]; then
 			HOST=$(echo $DOMAIN_WITH_CERT | cut -d. -f1)
@@ -38,6 +31,15 @@ for SECRET_NAME in $SECRET_NAMES; do
 			AFD=$(az network dns record-set list --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --query "[?name=='@'].targetResource.id" | jq -r '.|join("")|split("/")[-1]')
 
 		fi
+		OLD_FRONTENDS=$(az network front-door frontend-endpoint list --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD | jq -r '[.[].name]|join(" ")' )
+
+		FRONTENDPOINT="$(echo $DOMAIN_WITH_CERT | tr "." "-")-frontend-endpoint"
+		if [[ $OLD_FRONTENDS =~ (^|[[:space:]])$FRONTENDPOINT($|[[:space:]]) ]]; then
+			echo -e "\t$DOMAIN_WITH_CERT in Front Door, skipping\n"
+			continue
+		fi
+
+
 
 		echo -e "\tAdding domain $DOMAIN_WITH_CERT to Front Door\n"
 		az network front-door frontend-endpoint create --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $FRONTENDPOINT --host-name $DOMAIN_WITH_CERT --output $OUTPUT
@@ -46,9 +48,12 @@ for SECRET_NAME in $SECRET_NAMES; do
 	done
 done
 
-echo -e "\nUPDATING AZURE FRONT DOOR ROUTING RULES"
-AFD_FRONTENDS=$(az network front-door frontend-endpoint list --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD | jq -r '[.[].name]|join(" ")' )
-for RULE in $AFD_ROUTINGRULES; do
-	echo -e "\tAdding ALL endpoints/domains to rule: $RULE"
-	az network front-door routing-rule update --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $RULE --frontend-endpoints $AFD_FRONTENDS  --output $OUTPUT
+
+for door in p-wordpress-fd01 p-wordpress-fd02 p-wordpress-fd03 p-wordpress-fd04 p-wordpress-fd05 p-wordpress-fd06; do
+	echo -e "\nUPDATING AZURE FRONT DOOR ROUTING RULES"
+	AFD_FRONTENDS=$(az network front-door frontend-endpoint list --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $door | jq -r '[.[].name]|join(" ")' )
+	for RULE in $AFD_ROUTINGRULES; do
+		echo -e "\tAdding ALL endpoints/domains to rule: $RULE"
+		az network front-door routing-rule update --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $door --name $RULE --frontend-endpoints $AFD_FRONTENDS  --output $OUTPUT
+	done
 done
