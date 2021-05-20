90bcd2c8782feb588f7f4d2e52cda4f812b6aa92
Per Søderlind <per@soderlind.no>
2021-05-21 00:45:39 +0200

    add

--- add-afd.sh
+++ add-afd.sh
@@ -14,53 +14,7 @@ az config set extension.use_dynamic_install=yes_without_prompt
 KV_ID=$(az keyvault list --subscription "$SUBSCRIPTION" --resource-group $RG  | jq -r '[.[].id]|join("")')
 OLD_FRONTENDS=$(az network front-door frontend-endpoint list --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD | jq -r '[.[].name]|join(" ")' )
 DNS_ZONES=$(az network dns zone list --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --query '[].name' | jq -r '.|join(" ")')
-i=0
 
-echo -e "\nUPDATING AZURE DNS"
-SECRET_NAMES=$(az keyvault certificate list --vault-name $KV | jq -r '[.[].name]|join(" ")')
-for SECRET_NAME in $SECRET_NAMES; do
-	CUSTOMDOMAINS=$(az keyvault certificate show --vault-name $KV --name $SECRET_NAME | jq -r '.. | objects | select(.subjectAlternativeNames).subjectAlternativeNames.dnsNames |join(" ")')
-
-	for DOMAIN in  $CUSTOMDOMAINS; do
-		echo -e "\nUpdating $DOMAIN"
-		ZONE=$(echo $DOMAIN | rev | cut -d. -f1-2 | rev)
-
-		# If the domain in the certificate is in our Azure DNS, modify it.
-		if [[ $DNS_ZONES =~ (^|[[:space:]])$ZONE($|[[:space:]]) ]]; then
-
-			FRONTENDPOINT="$(echo $DOMAIN | tr "." "-")-frontend-endpoint"
-			if [[  $OLD_FRONTENDS =~ (^|[[:space:]])$FRONTENDPOINT($|[[:space:]]) ]]; then
-				echo -e "\tSkipping, $DOMAIN already in AFD"
-				continue
-			fi
-
-			i=$((i+1))
-			AFD_NR=$(($i%6 + 1))
-			AFD_HOST="p-wordpress-fd0$AFD_NR"
-			AFD_ID=$(az network front-door show --subscription "$SUBSCRIPTION" --resource-group $RG --name $AFD_HOST --query id -o tsv)
-
-			if [[ $ZONE != $DOMAIN ]]; then
-				HOST=$(echo $DOMAIN | cut -d. -f1)
-				echo -e "\tHOST: finding record type for $HOST in zone $ZONE"
-				record_type=$(az network dns record-set list --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --query "[?name=='$HOST'].type" | jq -r '.|join("")|split("/")[-1]' )
-				echo -e "\tRecord type: $record_type"
-				if [[ "A" == $record_type ]]; then
-					echo -e "\tDELETE A-record"
-					az network dns record-set a delete --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --name $HOST --yes --output $OUTPUT
-				fi
-				echo -e "\tCreate CNAME $HOST -> $AFD_HOST"
-				az network dns record-set cname set-record --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --record-set-name $HOST --cname $AFD_HOST --output $OUTPUT
-			elif [[ $BLACK_LIST =~ (^|[[:space:]])$ZONE($|[[:space:]]) ]]; then
-				echo -e "\tBLACKLISTED, DO NOTHING to $ZONE"
-			else
-				echo -e "\tAPEX domain, point @ to $AFD_HOST"
-				az network dns record-set a update --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --name "@"  --target-resource $AFD_ID --output $OUTPUT
-				echo -e "\tAdd CNAME afdverify -> afdverify.$AFD_HOST"
-				az network dns record-set cname set-record --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --record-set-name "afdverify" --cname "afdverify.${AFD_HOST}" --output $OUTPUT
-			fi
-		fi
-	done
-done
 
 echo -e "\nADDING DOMAINS TO AZURE FRONT DOOR"
 SECRET_NAMES=$(az keyvault certificate list --vault-name $KV | jq -r '[.[].name]|join(" ")')
