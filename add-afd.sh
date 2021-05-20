57022908a2129aac78269668f1deb7af25d45f93
Per Søderlind <per@soderlind.no>
2021-05-21 00:59:17 +0200

    Don't add existing domain

--- add-afd.sh
+++ add-afd.sh
@@ -22,6 +22,13 @@ for SECRET_NAME in $SECRET_NAMES; do
 	DOMAINS_WITH_CERTS=$(az keyvault certificate show --vault-name $KV --name $SECRET_NAME | jq -r '.. | objects | select(.subjectAlternativeNames).subjectAlternativeNames.dnsNames |join(" ")')
 	SECRET_ID=$(az keyvault certificate show --vault-name $KV --name $SECRET_NAME |jq  -r '[.sid]|join("")|split("/")[-1]')
 	for DOMAIN_WITH_CERT in $DOMAINS_WITH_CERTS; do
+
+		FRONTENDPOINT="$(echo $DOMAIN_WITH_CERT | tr "." "-")-frontend-endpoint"
+		if [[ $OLD_FRONTENDS =~ (^|[[:space:]])$FRONTENDPOINT($|[[:space:]]) ]]; then
+			echo -e "\t$DOMAIN_WITH_CERT in Front Door, skipping\n"
+			continue
+		fi
+
 		ZONE=$(echo $DOMAIN_WITH_CERT | rev | cut -d. -f1-2 | rev)
 		if [[ $ZONE != $DOMAIN_WITH_CERT ]]; then
 			HOST=$(echo $DOMAIN_WITH_CERT | cut -d. -f1)
@@ -30,18 +37,11 @@ for SECRET_NAME in $SECRET_NAMES; do
 			AFD=$(az network dns record-set list --subscription "$SUBSCRIPTION" --resource-group $DNS_RG --zone-name $ZONE --query "[?name=='@'].targetResource.id" | jq -r '.|join("")|split("/")[-1]')
 			AFD="$AFD.azurefd.net"
 		fi
-		is_validated_domain=$(az network front-door check-custom-domain --subscription "$SUBSCRIPTION" --resource-group $RG  --name $AFD --host-name $DOMAIN_WITH_CERT --query customDomainValidated)
-		if [[ "true" == $is_validated_domain  ]]; then
-			FRONTENDPOINT="$(echo $DOMAIN_WITH_CERT | tr "." "-")-frontend-endpoint"
-			if [[ ! $OLD_FRONTENDS =~ (^|[[:space:]])$FRONTENDPOINT($|[[:space:]]) ]]; then
 
-				echo -e "\tAdding domain $DOMAIN_WITH_CERT to Front Door\n"
-				az network front-door frontend-endpoint create --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $FRONTENDPOINT --host-name $DOMAIN_WITH_CERT --output $OUTPUT
-				az network front-door frontend-endpoint enable-https --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $FRONTENDPOINT --vault-id $KV_ID --certificate-source AzureKeyVault --secret-name $SECRET_NAME --secret-version $SECRET_ID --output $OUTPUT
-			else
-				echo -e "\tDomain $DOMAIN_WITH_CERT with frontend endpoit $FRONTENDPOINT allreary exists in $AFD\n"
-			fi
-		fi
+		echo -e "\tAdding domain $DOMAIN_WITH_CERT to Front Door\n"
+		az network front-door frontend-endpoint create --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $FRONTENDPOINT --host-name $DOMAIN_WITH_CERT --output $OUTPUT
+		az network front-door frontend-endpoint enable-https --subscription "$SUBSCRIPTION" --resource-group $RG --front-door-name $AFD --name $FRONTENDPOINT --vault-id $KV_ID --certificate-source AzureKeyVault --secret-name $SECRET_NAME --secret-version $SECRET_ID --output $OUTPUT
+
 	done
 done
 
